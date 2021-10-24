93f6fbfa7e0ded86e25c87722e52264cca6c00c7
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-10-23 21:57:49 -0300

    📕  class 4 - config react testing library for cypress

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,7 +1,17 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to google', () => {
+  it.skip('should go to google', () => {
     cy.google()
+  })
+
+  it('should change light/dark theme on willian justen site', () => {
+    cy.visit('https://willianjusten.com.br')
+
+    cy.findByTitle(/mudar o tema/i).click()
+    cy.get('.light').should('exist')
+
+    cy.findByTitle(/mudar o tema/i).click()
+    cy.get('.dark').should('exist')
   })
 })
