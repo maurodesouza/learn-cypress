a83860f1aca6067a2fee67c852a8688c5d71d287
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-10-24 02:53:38 -0300

    📗  class 11 - click and wait

--- client/cypress/e2e/home.ts
+++ client/cypress/e2e/home.ts
@@ -4,9 +4,21 @@ describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
 
-    cy.get('.slick-slide').within(() => {
+    cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /cyberpunk 2077/i })
       cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /swat 4: gold edition/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /metal gear!/i })
+      cy.findByRole('link', { name: /browse games/i })
     })
   })
 })
