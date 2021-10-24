ffecb5fc90019ae615dd2e7d86ca0a1a71beca5b
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-10-24 16:45:49 -0300

    📙  class 19 - solve exercises

--- client/cypress/e2e/game.ts
+++ client/cypress/e2e/game.ts
@@ -17,5 +17,29 @@ describe('Game Page', () => {
     cy.getByDataCy('content').within(() => {
       cy.findByRole('heading', { name: 'Description' })
     }).children().should('have.length.at.least', 2)
+
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i })
+
+      cy.findByRole('heading', { name: /developer/i })
+      cy.findByRole('heading', { name: /publisher/i })
+
+      cy.findByRole('heading', { name: /release date/i })
+      cy.findByRole('heading', { name: /rating/i })
+
+      cy.findByRole('heading', { name: /platforms/i })
+      cy.findByRole('heading', { name: /genres/i })
+
+      cy.findAllByText(/cd projekt red/i).should('have.length', 2)
+      cy.findByText(/dec 8, 2020/i).should('exist')
+
+      cy.findByText(/free/i).should('exist')
+      cy.findByText(/card game \/ action \/ sci-fi/i).should('exist')
+
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+    })
+
+    cy.shouldRenderShowcase({ name: 'Upcoming Games', hightlight: true  })
+    cy.shouldRenderShowcase({ name: 'Recomendados', hightlight: false  })
   })
 })
