1d36ff9996c6f5b39d17eaafaa886d5fd2fc7de7
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-10-24 16:05:06 -0300

    📘  class 17 - first tests in game page

---
+++ client/cypress/e2e/game.ts
@@ -0,0 +1,17 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Game Page', () => {
+  it('should render home sections', () => {
+    cy.visit('/game/cyberpunk-2077')
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+      cy.findByText(/^cyberpunk 2077 is an open-world/i).should('exist')
+
+      cy.findByText(/\$199.90/i).should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
+  })
+})
