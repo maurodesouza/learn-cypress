99cc562371d1fa37db125a4e213f76b0326980f5
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-10-30 15:32:29 -0300

    📗  class 32 - tests to sign in page

--- client/cypress/e2e/user.ts
+++ client/cypress/e2e/user.ts
@@ -3,16 +3,28 @@
 import { createUser } from '../support/generate'
 
 describe('User', () => {
-  before(() => {
-    cy.visit('/sign-up')
-  })
-
   it('should sign up', () => {
-    const user = createUser()
+    cy.visit('/sign-up')
 
+    const user = createUser()
     cy.signUp(user)
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
+  })
+
+  it('should sign in', () => {
+    cy.visit('/sign-in')
+
+    cy.findByPlaceholderText(/email/i).type('e2e@wongames.com')
+    cy.findByPlaceholderText(/^password/i).type('cypress')
+
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
   })
 })
