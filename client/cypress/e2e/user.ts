fd4510042ab3f009b730f8b41191e51243531720
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-10-30 16:34:55 -0300

    📔  class 34 - tests to auth routes and redirects

--- client/cypress/e2e/user.ts
+++ client/cypress/e2e/user.ts
@@ -25,4 +25,15 @@ describe('User', () => {
     cy.findByRole('link', { name: /sign in/i }).should('exist')
     cy.findByText(/cypress/i).should('not.exist')
   })
+
+  it('should sign in the user and redirect to the page that it was defined previously', () => {
+    cy.visit('/profile/me')
+    cy.urlShouldBeEqualTo('/sign-in?callbackUrl=/profile/me')
+
+    cy.signIn()
+    cy.urlShouldBeEqualTo('/profile/me')
+
+    cy.findByPlaceholderText(/username/i).should('have.value', 'cypress')
+    cy.findByPlaceholderText(/e-mail/i).should('have.value', 'e2e@wongames.com')
+  })
 })
